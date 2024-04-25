3bfe4a32f98100c1c3404ea06abd2e3aa65ee162
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-04-25 16:06:36 +0200

    Refactor thermo.py to improve command line argument parsing and logging

--- ThermoScreening/cli/thermo.py
+++ ThermoScreening/cli/thermo.py
@@ -1,12 +1,9 @@
 import numpy as np
 from argparse import ArgumentParser
 from ThermoScreening.thermo.api import execute
-import logging
 import time
-import sys
 from ..__version__ import __version__
 
-logger = logging.getLogger(__name__)
 
 def parse_args():
     """
@@ -16,16 +13,10 @@ def parse_args():
     args: argparse.Namespace: command line arguments
     """
     parser = ArgumentParser(description="ThermoScreening")
-    parser.add_argument("input", type=str, help="Input file")
-    # output file is optional and defaults to STDOUT
-    parser.add_argument(
-        "-o", "--output", type=str, default=sys.stdout, help="Output file"
-    )
-    parser.add_argument("-p", "--plot", type=str, default="plot.png", help="Plot file")
+    parser.add_argument("input_file", type=str, help="Input file")
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="Verbose output", default=True
     )
-    parser.add_argument("-t", "--test", action="store_true", help="Test run")
     args = parser.parse_args()
     return args
 
@@ -43,31 +34,21 @@ def main():
     """
     parser_args = parse_args()
     input_file = parser_args.input
-    output_file = parser_args.output
-    plot_file = parser_args.plot
     verbose = parser_args.verbose
-    test = parser_args.test
 
-
-    # use logging to print verbose output if verbose is True
-    # if verbose is False, do not print verbose output
     if verbose:
-        logging.basicConfig(level=logging.INFO)
-        logging.info(f"Input file: {input_file}")
-        logging.info(f"Output file: {output_file}")
-        logging.info(f"Plot file: {plot_file}")
-        logging.info(f"Verbose: {verbose}")
-        logging.info(f"Test: {test}")
-        logging.info(f"Version: {__version__}")
-        start = time.time()
+        print("Input file: ", input_file)
+        print("Verbose: ", verbose)
 
-    # Execute main program
+    # start timer
+    start = time.time()
+    # Execute
     execute(input_file)
-    
-    # Print time elapsed if verbose is True using logging
-    if verbose:
-        end = time.time()
-        logging.info(f"Time elapsed: {end - start:.2f} seconds")
+
+    # end timer
+    end = time.time()
+    print("#####################################\n")
+    print("Time elapsed: ", end - start, " s")
 
 
 if __name__ == "__main__":
