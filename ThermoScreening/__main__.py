d710ff6eaa5b648f82024651caf59a2f56716407
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-04-25 14:13:20 +0200

    Add ThermoScreening __main__.py module and update cli/__init__.py

---
+++ ThermoScreening/__main__.py
@@ -0,0 +1,11 @@
+"""
+This module is the main entry point for the application.
+
+It is responsible for calling the main function in the thermo module.
+
+Example:
+    $ python -m ThermoScreening
+"""
+from cli import main
+
+main()
