a23d9ba486fdd5a7576a0d3b36bbaa99f115e7c9
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-05-10 22:52:39 +0200

    Removed dftb+ and modes checks in __init__ calculator

--- ThermoScreening/calculator/__init__.py
+++ ThermoScreening/calculator/__init__.py
@@ -1,11 +1,4 @@
 import os
 
-# without dftb+ and modes, the module is useless
-if os.system("which dftb+ > /dev/null") != 0:
-    raise FileNotFoundError("The dftb+ executable is not found.")
-
-if os.system("which modes > /dev/null") != 0:
-    raise FileNotFoundError("The modes executable is not found.")
-
 # import the classes
 from .dftbplus import Geoopt, Hessian, Modes
